eeb8c8fbf5e0ff534d2dae49c9b00e347819355f
pranav kumar <kumaarpranv@gmail.com>
2022-11-23 02:57:52 +0530

    cursor dialog slow only keymap

--- lua/pranv/remap.lua
+++ lua/pranv/remap.lua
@@ -1,5 +1,6 @@
 local nnoremap = require("pranv.keymap").nnoremap
 local inoremap = require("pranv.keymap").inoremap
 
-nnoremap("<leader>e", "<cmd>NvimTreeToggle<CR>")
+nnoremap("<leader>x", "<cmd>NvimTreeToggle<CR>")
 inoremap("<cmd>v", "<Esc>pi")
+nnoremap("<leader>e", "<cmd>lua vim.diagnostic.open_float(nil, {focus=false})<CR>")
