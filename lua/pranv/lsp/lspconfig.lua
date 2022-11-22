00ab694a02f54cf828532ed364334187a33d92f8
pranav kumar <kumaarpranv@gmail.com>
2022-11-22 23:53:42 +0530

    adding clangd

--- lua/pranv/lsp/lspconfig.lua
+++ lua/pranv/lsp/lspconfig.lua
@@ -56,3 +56,8 @@ lspconfig["gopls"].setup({
   capabilities = capabilities,
   on_attach = on_attach,
 })
+
+lspconfig["clangd"].setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
--- lua/pranv/lsp/mason.lua
+++ lua/pranv/lsp/mason.lua
@@ -12,6 +12,7 @@ mason_lspconfig.setup({
     "gopls",
     "html",
     "emmet_ls",
+    "clangd",
   },
   -- auto-install configured servers (with lspconfig)
   automatic_installation = true, -- not the same as ensure_installed
