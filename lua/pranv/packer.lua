3e635a5883c664d7bb66495d15083a2cd9933917
pranav kumar <kumaarpranv@gmail.com>
2022-12-28 13:21:30 +0530

    removed neogit, added readme

--- lua/pranv/packer.lua
+++ lua/pranv/packer.lua
@@ -6,7 +6,6 @@ vim.cmd.packadd('packer.nvim')
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-  use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
 
   --utils
   use 'nvim-lua/plenary.nvim'
