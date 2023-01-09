bf32a9691976460fccd51e725147753059c1a38e
pranav kumar <kumaarpranv@gmail.com>
2023-01-10 00:07:29 +0530

    added dap python with win32

--- after/plugin/nvim-dap-python.lua
+++ after/plugin/nvim-dap-python.lua
@@ -1,7 +1,24 @@
+local ok_dap, dap = pcall(require, "dap")
 local ok, dappython = pcall(require, "dap-python")
-if not ok then
+if (not ok) or (not ok_dap) then
   return
 end
-
-dappython.setup('C:\\Users\\kumaa\\miniconda3\\python')
+local python_path = ""
+if vim.fn.has('win32') then
+  python_path = "C:\\Users\\kumaa\\miniconda3\\python.exe"
+else
+  python_path = ""
+end
+dap.configurations.python = {
+  {
+    type = 'python';
+    request = 'launch';
+    name = "Launch file";
+    program = "${file}";
+    pythonPath = function()
+      return python_path
+    end;
+  },
+}
+dappython.setup(python_path)
 -- dappython.test_runner = 'pytest'
