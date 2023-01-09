c280691913583db40c336ccb7b286e903c969557
pranav kumar <kumaarpranv@gmail.com>
2023-01-09 21:43:21 +0530

    added neotree

--- lua/pranv/packer.lua
+++ lua/pranv/packer.lua
@@ -53,7 +53,15 @@ return require('packer').startup(function(use)
   use { "catppuccin/nvim", as = "catppuccin" }
 
   -- explorer
-  -- use 'nvim-tree/nvim-tree.lua'
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
 
   -- icons
   use 'nvim-tree/nvim-web-devicons'
